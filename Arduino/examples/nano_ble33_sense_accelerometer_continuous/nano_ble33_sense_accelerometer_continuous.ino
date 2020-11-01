e5cfffc8eec5bc3bd87705d54f05c863b79d3c72
vishwasnavada <vishwasnavada@gmail.com>
2020-11-01 18:12:17 +0530

    Added trained model arduino ouput

---
+++ Arduino/examples/nano_ble33_sense_accelerometer_continuous/nano_ble33_sense_accelerometer_continuous.ino
@@ -0,0 +1,161 @@
+/* Edge Impulse Arduino examples
+ * Copyright (c) 2020 EdgeImpulse Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* Includes ---------------------------------------------------------------- */
+#include <gaja_kanti_inference.h>
+#include <Arduino_LSM9DS1.h>
+
+/* Constant defines -------------------------------------------------------- */
+#define CONVERT_G_TO_MS2    9.80665f
+
+/* Private variables ------------------------------------------------------- */
+static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+static uint32_t run_inference_every_ms = 200;
+static rtos::Thread inference_thread(osPriorityLow);
+static float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
+
+/**
+* @brief      Arduino setup function
+*/
+void setup()
+{
+    delay(5000);
+
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("Edge Impulse Inferencing Demo");
+
+    if (!IMU.begin()) {
+        ei_printf("Failed to initialize IMU!\r\n");
+    }
+    else {
+        ei_printf("IMU initialized\r\n");
+    }
+
+    if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != 3) {
+        ei_printf("ERR: EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME should be equal to 3 (the 3 sensor axes)\n");
+        return;
+    }
+
+    inference_thread.start(&run_inference);
+}
+
+/**
+* @brief      Printf function uses vsnprintf and output using Arduino Serial
+*
+* @param[in]  format     Variable argument list
+*/
+void ei_printf(const char *format, ...) {
+   static char print_buf[1024] = { 0 };
+
+   va_list args;
+   va_start(args, format);
+   int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
+   va_end(args);
+
+   if (r > 0) {
+       Serial.write(print_buf);
+   }
+}
+
+/**
+ * @brief      Run inferencing in the background.
+ *             You probably want to implement some averaging here, e.g.
+ *             look at the last 10 inference results and only do something
+ *             when 70% of the last inferences are the same.
+ */
+void run_inference()
+{
+    // wait until we have a full buffer
+    delay((EI_CLASSIFIER_INTERVAL_MS * EI_CLASSIFIER_RAW_SAMPLE_COUNT) + 100);
+
+    while (1) {
+        // copy the buffer
+        float inference_buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
+        memcpy(inference_buffer, buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE * sizeof(float));
+
+        // Turn the raw buffer in a signal which we can the classify
+        signal_t signal;
+        int err = numpy::signal_from_buffer(inference_buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+        if (err != 0) {
+            ei_printf("Failed to create signal from buffer (%d)\n", err);
+            return;
+        }
+
+        // Run the classifier
+        ei_impulse_result_t result = { 0 };
+
+        err = run_classifier(&signal, &result, debug_nn);
+        if (err != EI_IMPULSE_OK) {
+            ei_printf("ERR: Failed to run classifier (%d)\n", err);
+            return;
+        }
+
+        // print the predictions
+        ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+            result.timing.dsp, result.timing.classification, result.timing.anomaly);
+        for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+            ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
+        }
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+        ei_printf("    anomaly score: %.3f\n", result.anomaly);
+#endif
+
+        delay(run_inference_every_ms);
+    }
+}
+
+/**
+* @brief      Get data and run inferencing
+*
+* @param[in]  debug  Get debug info if true
+*/
+void loop()
+{
+    while (1) {
+        // Determine the next tick (and then sleep later)
+        uint64_t next_tick = micros() + (EI_CLASSIFIER_INTERVAL_MS * 1000);
+
+        // roll the buffer -3 points so we can overwrite the last one
+        numpy::roll(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, -3);
+
+        // read to the end of the buffer
+        IMU.readAcceleration(
+            buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3],
+            buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 2],
+            buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 1]
+        );
+
+        buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3] *= CONVERT_G_TO_MS2;
+        buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 2] *= CONVERT_G_TO_MS2;
+        buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 1] *= CONVERT_G_TO_MS2;
+
+        // and wait for next tick
+        uint64_t time_to_wait = next_tick - micros();
+        delay((int)floor((float)time_to_wait / 1000.0f));
+        delayMicroseconds(time_to_wait % 1000);
+    }
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_ACCELEROMETER
+#error "Invalid model for current sensor"
+#endif
